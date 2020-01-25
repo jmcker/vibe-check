72b35a4b632c6dfe5ce3097a27a78bbae08436ab
Charlene Orr <charlorrnot@gmail.com>
2020-01-25 05:54:11 -0500

    Add map to main view

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,8 @@
 import UIKit
 import Flutter
 
+import GoogleMaps
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
@@ -8,6 +10,7 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("AIzaSyC6W6LLpYLd7ds9ekdK4mCLlZKl33-oCfk")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }

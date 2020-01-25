80f895da5ef04f7c5ad7338d2c70ec654a9cfdb4
unknown <aesonakhras@gmail.com>
2020-01-25 18:06:46 -0500

    Commuication with Spotify API

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,113 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:http/http.dart';
+import 'package:spotify/spotify_io.dart';
+
+const kAndroidUserAgent =
+    'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Mobile Safari/537.36';
+
+
+
+
+
+String spotify = 'https://accounts.spotify.com/authorize' +
+  '?response_type=code' +
+  '&client_id=' + "c451eab95a624bfbb8cf67a84b11b985" +
+  ('&scope=user-read-recently-played') +
+  '&redirect_uri=' + "https://vibecheck.tk/auth" + 
+  "&show_dialog=true";
+
+
+String spotify_uri = Uri.encodeFull(spotify);
+
+String selectedUrl = spotify_uri;
+
+// ignore: prefer_collection_literals
+final Set<JavascriptChannel> jsChannels = [
+  JavascriptChannel(
+      name: 'Print',
+      onMessageReceived: (JavascriptMessage message) {
+        print(message.message);
+      }),
+].toSet();
+
+
+String code = 'Blank';
+String access_token = "";
+String refresh_token = "";
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final flutterWebViewPlugin = FlutterWebviewPlugin();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter WebView Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {
+        '/': (_) => const MyHomePage(title: 'Flutter WebView Demo'),
+        '/widget': (_) {
+          return WebviewScaffold(
+            url: selectedUrl,
+            javascriptChannels: jsChannels,
+            mediaPlaybackRequiresUserGesture: false,
+            appBar: AppBar(
+              title: const Text('Widget WebView'),
+            ),
+            withZoom: true,
+            withLocalStorage: true,
+            hidden: true,
+            initialChild: Container(
+              color: Colors.redAccent,
+              child: const Center(
+                child: Text('Waiting.....'),
+              ),
+            ),
+            bottomNavigationBar: BottomAppBar(
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                    icon: const Icon(Icons.arrow_back_ios),
+                    onPressed: () {
+                      flutterWebViewPlugin.goBack();
+                    },
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.arrow_forward_ios),
+                    onPressed: () {
+                      flutterWebViewPlugin.goForward();
+                    },
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.autorenew),
+                    onPressed: () {
+                      flutterWebViewPlugin.reload();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      },
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
 
@@ -44,68 +116,193 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // Instance of WebView plugin
+  final flutterWebViewPlugin = FlutterWebviewPlugin();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  // On destroy stream
+  StreamSubscription _onDestroy;
+
+  // On urlChanged stream
+  StreamSubscription<String> _onUrlChanged;
+
+  // On urlChanged stream
+  StreamSubscription<WebViewStateChanged> _onStateChanged;
+
+  StreamSubscription<WebViewHttpError> _onHttpError;
+
+  StreamSubscription<double> _onProgressChanged;
+
+  StreamSubscription<double> _onScrollYChanged;
+
+  StreamSubscription<double> _onScrollXChanged;
+
+  final _urlCtrl = TextEditingController(text: selectedUrl);
+
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+
+    flutterWebViewPlugin.close();
+
+    _urlCtrl.addListener(() {
+      selectedUrl = _urlCtrl.text;
+    });
+
+    // Add a listener to on destroy WebView, so you can make came actions.
+    _onDestroy = flutterWebViewPlugin.onDestroy.listen((_) {
+      if (mounted) {
+        // Actions like show a info toast.
+        _scaffoldKey.currentState.showSnackBar(
+            const SnackBar(content: const Text('Webview Destroyed')));
+      }
+    });
+
+    // Add a listener to on url changed
+    _onUrlChanged = flutterWebViewPlugin.onUrlChanged.listen((String url) async {
+      if (mounted) {
+          int startIndex = url.indexOf('code=');
+          if (startIndex != -1) {
+            code = url.substring(startIndex+5);
+            Response res = await get("https://vibecheck.tk/api/auth?code=" + code);
+            if (res.statusCode == 200) {
+              Map<String, dynamic> map = jsonDecode(res.body);
+              access_token = map["access_token"];
+              print(access_token);
+              Map<String, String> requestHeaders = {
+                'Authorization': 'Bearer ' + access_token,
+              };
+              Response res2 = await get("https://api.spotify.com/v1/me/player/recently-played", headers:requestHeaders);
+              Map<String, dynamic> map2 = jsonDecode(res2.body);
+
+              List <dynamic> items = map2["items"];
+
+              Map<String, dynamic> vibes = new Map();
+              vibes["latitude"] = "40.4285323";
+              vibes["longitude"] = "-86.9240971";
+
+              List<dynamic> tracks = new List();
+              
+              //Need to change this to size of vibes were getting
+              for (var i = 0; i < items.length; i++) {
+                Map<String, dynamic> track = new Map();
+                track["artist"] = items[i]["track"]["artists"][0]["name"];
+                track["album"] = items[i]["track"]["album"]["name"];
+                track["artist_id"] = items[i]["track"]["artists"][0]["id"];
+                track["popularity"] = items[i]["track"]["popularity"];
+                track["track_id"] = items[i]["track"]["id"];
+                track["title"] = items[i]["track"]["name"];
+                tracks.add(track);
+              }
+
+              //Now put all of the artist id's in an array to send to get genre
+              String artistCommaString = "";
+              for (var i = 0; i < items.length - 1; i++) {
+                artistCommaString += tracks[i]["artist_id"] + ',';
+              }
+
+              artistCommaString += tracks[items.length-1]["artist_id"];
+
+              Response artistResponse = await get(Uri.encodeFull("https://api.spotify.com/v1/artists?ids=" + artistCommaString), headers:requestHeaders);
+              if (res.statusCode == 200) {
+                Map<String, dynamic> artistMap = jsonDecode(artistResponse.body);
+                print(artistMap);
+                List <dynamic> artistList = artistMap["artists"];
+
+                for (int i  = 0; i < artistList.length; i++) {
+                  tracks[i]["genre"] = artistList[i]["genres"][0];
+                }
+
+                vibes["tracks"] = tracks;
+                Map<String, String> vibeHeaders = {
+                  'Content-Type': 'application/json',
+                };
+                Response vibePost = await post("https://vibecheck.tk/api/vibe", headers:vibeHeaders ,body:json.encode(vibes));
+                if (vibePost.statusCode == 200) {
+                  print("Vibes have been posted");
+                }
+                else {
+                  print(vibePost.body);
+                }
+              }
+              else {
+                print(jsonDecode(artistResponse.body));
+              }
+            }
+          }
+        setState((){
+        });
+      }
+    });
+
+    _onProgressChanged =
+        flutterWebViewPlugin.onProgressChanged.listen((double progress) {
+      if (mounted) {
+        setState(() {
+        });
+      }
+    });
+
+    _onStateChanged =
+        flutterWebViewPlugin.onStateChanged.listen((WebViewStateChanged state) {
+      if (mounted) {
+        setState(() {
+        });
+      }
+    });
+
+    _onHttpError =
+        flutterWebViewPlugin.onHttpError.listen((WebViewHttpError error) {
+      if (mounted) {
+        setState(() {
+        });
+      }
     });
   }
 
   @override
+  void dispose() {
+    // Every listener should be canceled, the same should be done with this stream.
+    _onDestroy.cancel();
+    _onUrlChanged.cancel();
+    _onStateChanged.cancel();
+    _onHttpError.cancel();
+    _onProgressChanged.cancel();
+
+    flutterWebViewPlugin.dispose();
+    super.dispose();
+    
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Plugin example app'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          children: [
+            Container(
+              padding: const EdgeInsets.all(24.0),
+              child: TextField(controller: _urlCtrl),
+            ),
+            RaisedButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed('/widget');
+              },
+              child: const Text('Open widget webview'),
             ),
             Text(
-              '$_counter',
+              '$code',
               style: Theme.of(context).textTheme.display1,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
